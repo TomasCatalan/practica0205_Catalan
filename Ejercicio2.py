6248312635604a62f0ab5150de40f92e707d52ae
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-14 16:50:55 +0100

    Update Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -15,6 +15,15 @@ n = int(input("introduzca un numero entero"))
 print(x(n))
 
 def y(m): # BUCLE ITERATIVO
+    '''Funcion que calcula el factorian de un numero entero
+    con un bucle
+    
+    Parametros
+        - n: un numero entero introducido
+        
+    Salida:
+        - factorial del numero introducido'''
+    
     for i in range(1, m + 1):
         z = z * (z -1)  
     return z
