fd9a495ecd779155626ab124d6c97e4cacd3a592
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-14 17:06:27 +0100

    Update Ejercicio3.py

---
+++ Ejercicio3.py
@@ -0,0 +1,16 @@
+def x(y):
+
+    return 3.1416 * y**2
+
+y = int(input("introduzca un numero entero para el radio"))
+print("el area del circulo es", x(y))
+
+def a(y, b):
+
+    d = x(y)
+    c = d * b
+    return c
+b = int(input("introduzca un numero entero pera la altura"))
+
+print("el volumen del cilindro es de", a(y, b))
+
