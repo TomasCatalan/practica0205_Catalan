49b3f756c39146d7ce9f5b98037bff2af07eeee5
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-14 17:23:44 +0100

    Update Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,23 +1,30 @@
-def x(y):
+def x(radio):
     '''Funcion que calcula el area de un circulo
     
     Parametros:
-        - y: radio del circulo
+        - radio: radio del circulo
     
     Salida:
         - Entrega el area de un circulo'''
 
-    return 3.1416 * y**2
+    return 3.1416 * radio**2
 
-y = int(input("introduzca un numero entero para el radio"))
-print("el area del circulo es", x(y))
+radio = int(input("introduzca un numero entero para el radio"))
+print("el area del circulo es", x(radio))
 
-def a(y, b):
-
-    d = x(y)
-    c = d * b
+def a(radio, altura):
+    '''Funcion que calcula el area de un cilindro, 
+    
+    Parametros:
+        - radio: radio del circulo
+        - altura: altura del cilindro
+    
+    Salida:
+        - Entrega el area de un cilindro'''
+ 
+    c = x(radio) * altura
     return c
-b = int(input("introduzca un numero entero para la altura"))
+altura = int(input("introduzca un numero entero para la altura"))
 
-print("el volumen del cilindro es de", a(y, b))
+print("el volumen del cilindro es de", a(radio, altura))
 
