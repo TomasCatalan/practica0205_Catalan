e7904c80caa4e77071951031bc4f2ba32aef7030
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-14 18:37:37 +0100

    Update Ejercicio4.py

---
+++ Ejercicio4.py
@@ -0,0 +1,14 @@
+def media():
+    x = []
+    while True:
+       try:
+            y = float(input("introduce un numero"))
+            x.append(y)
+       except ValueError:
+           break
+    
+    if x:
+        z = sum(x) / len(x)
+        print("la media de", x, "es igual a",z)
+
+media()
