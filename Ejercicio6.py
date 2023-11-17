6a55c45b5df2aa9a4a4ed19fe43debf23de0af59
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-17 11:38:38 +0100

    Update Ejercicio6.py

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,8 +1,17 @@
 def decimalbinario(decimal):
+    '''Funcion que transforma un numero decimal a binario
+    
+    Parametros:
+        - x: transforma la parte entera del numero decimal introducido
+        - xComa: almacena la parte decimal del numero
+        - decimal: almacena el numero decimal
 
-    x = bin(int(decimal))[2:]
+    Salida
+        - Devuelve el numero decimal convertido a binario
+        '''
+    x = bin(int(decimal))[2:] #el 2: elimina el prefijo de binario (Ob)
     xComa = decimal - int(decimal)
-    for _ in range(int(decimal + 1)):
+    for i in range(int(decimal + 1)):
             xComa *= 2
             x += str(int(xComa))
             xComa -= int(xComa)
@@ -10,5 +19,22 @@ def decimalbinario(decimal):
     print(x)
     return x
 
-x= 45.89763
+x = 45.87653
 decimalbinario(x)
+
+def binariodecimal(binario):
+    '''Funcion que transforma un binario a decimal
+    
+    Parametros:
+        - binario: se refiere al numero binario introducido en la funcion
+    
+    Salida:
+        - devuelve el binario convertido a decimal'''
+
+    binario = int(binario, 2) # int(num binario, 2)
+    
+    print(float(binario))
+    return float(binario)
+    
+y = "10110101111010110"
+binariodecimal(y)
